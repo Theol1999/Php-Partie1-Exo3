50979f4b29a082d480f956c35934aaffbc7b5f7a
theoL <theo1007@outlook.fr>
2019-03-13 14:57:11 +0100

    Ajout de l'exo 3

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$km = 1;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+      echo $km
+      ?>
+    </p>
+    <p>
+      <?php
+      echo $km *3;
+      ?>
+    </p>
+    <p>
+      <?php
+      echo $km *125;
+      ?>
+    </p>
+  </body>
+</html>
